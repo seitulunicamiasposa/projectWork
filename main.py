546043ef60660b88093982484c46e1a7ce8df2c3
Antonio Manuel Carelli <acarelli@studenti.apuliadigitalmaker.it>
2024-11-27 17:52:35 +0100

    FASTAPI middleware

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from handler.autenticazione import router as autenticazione_router
 from handler.impianti import router as impianti_router
 from handler.macchinari import router as macchinari_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(  
 
@@ -12,6 +13,16 @@ app = FastAPI(
     redoc_url=None
 
 )
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(autenticazione_router)
 app.include_router(impianti_router)
 app.include_router(macchinari_router)
